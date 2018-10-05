31e86de71b52d31c0797deb78c42f5c656890095
Kevin Wolkober <kevin.wolkober@gmail.com>
2018-10-05 10:44:19 +0800

    Add ListyKit base framework
    - This framework should handle Foundation-driven elements and
    be reusable across all apps and extensions

---
+++ ListyKit/ListyKit.h
@@ -0,0 +1,18 @@
+//
+//  ListyKit.h
+//  ListyKit
+//
+//  Created by Kevin Wolkober on 10/5/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ListyKit.
+FOUNDATION_EXPORT double ListyKitVersionNumber;
+
+//! Project version string for ListyKit.
+FOUNDATION_EXPORT const unsigned char ListyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ListyKit/PublicHeader.h>
+
+
