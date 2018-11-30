1e1a0f86bd03d0f53abd18926316b1c98da2bba1
Kevin Wolkober <kevin.wolkober@gmail.com>
2018-11-30 10:16:25 +0800

    Add base macOS app

---
+++ Apps/Listy macOS/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Listy macOS
+//
+//  Created by Kevin Wolkober on 11/30/18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Apps/Listy macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Listy macOS
+//
+//  Created by Kevin Wolkober on 11/30/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
