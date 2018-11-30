3dbe3f2328e68f53f2eb44948e75a29a522e6800
Kevin Wolkober <kevin.wolkober@gmail.com>
2018-11-30 11:36:30 +0800

    Add base lists and tasks views

---
+++ Apps/Listy macOS/Lists/List/View/ListsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListsViewController.swift
+//  Listy macOS
+//
+//  Created by Kevin Wolkober on 11/30/18.
+//
+
+import Cocoa
+
+class ListsViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ Apps/Listy macOS/Tasks/List/View/TasksListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TasksListViewController.swift
+//  Listy macOS
+//
+//  Created by Kevin Wolkober on 11/30/18.
+//
+
+import Cocoa
+
+class TasksListViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+}
