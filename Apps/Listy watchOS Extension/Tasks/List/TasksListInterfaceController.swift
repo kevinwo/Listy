6f5bc6e0149e469348c1f62f0318b74f216228fc
Kevin Wolkober <kevin.wolkober@gmail.com>
2018-12-02 16:49:30 +0800

    [watch] Add tasks list view base

---
+++ Apps/Listy watchOS Extension/Tasks/List/TasksListInterfaceController.swift
@@ -0,0 +1,14 @@
+//
+//  TasksListInterfaceController.swift
+//  Listy watchOS Extension
+//
+//  Created by Kevin Wolkober on 12/2/18.
+//
+
+import WatchKit
+
+class TasksListInterfaceController: WKInterfaceController {
+
+    @IBOutlet weak var addTaskButton: WKInterfaceButton!
+    @IBOutlet weak var table: WKInterfaceTable!
+}
